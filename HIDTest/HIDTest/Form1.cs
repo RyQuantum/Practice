9ed6a0154aba083d5a1604a5cce5df723dcf5166
Ryan <ryan@rently.com>
2021-08-27 19:28:44 +0800

    create HIDTest project

---
+++ HIDTest/HIDTest/Form1.cs
@@ -0,0 +1,51 @@
+﻿using HidLibrary;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HIDTest
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            Func();
+        }
+
+        public void Func()
+        {
+            var HidDevice = HidDevices.Enumerate(0x1995, 0x0806).FirstOrDefault();
+
+            Debug.WriteLine("Connected: " + HidDevice.IsConnected.ToString());
+            byte[] OutData = new byte[HidDevice.Capabilities.OutputReportByteLength];
+
+            // Send a report to initiate an error sound
+            OutData[0] = 0x01;
+            OutData[1] = 0x02;
+            OutData[2] = 0x03;
+            OutData[3] = 0x04;
+            OutData[62] = 0x63;
+            OutData[63] = 0x64;
+            OutData[64] = 0x65;
+
+            HidDevice.Write(OutData);
+
+            // Blocking read of report
+            HidDeviceData InData = HidDevice.Read();
+            Debug.WriteLine(BitConverter.ToString(InData.Data).Replace("-", ""));
+        }
+    }
+
+}
---
+++ HIDTest/HIDTest/Form2.cs
@@ -0,0 +1,66 @@
+﻿using Device.Net;
+using Usb.Net.Windows;
+using Hid.Net.Windows;
+using Microsoft.Extensions.Logging;
+
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HIDTest
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            Main();
+        }
+
+        private static async Task Main()
+        {
+            //Create logger factory that will pick up all logs and output them in the debug output window
+            var loggerFactory = LoggerFactory.Create((builder) =>
+            {
+                _ = builder.AddDebug().SetMinimumLevel(LogLevel.Trace);
+            });
+
+            //----------------------
+
+            // This is Windows specific code. You can replace this with your platform of choice or put this part in the composition root of your app
+
+            //Register the factory for creating Hid devices. 
+            var hidFactory =
+                new FilterDeviceDefinition(vendorId: 0x1995, productId: 0x0806)
+                .CreateWindowsHidDeviceFactory(loggerFactory);
+
+            //----------------------
+
+            //Get connected device definitions
+            var deviceDefinitions = (await hidFactory.GetConnectedDeviceDefinitionsAsync().ConfigureAwait(false)).ToList();
+
+            if (deviceDefinitions.Count == 0)
+            {
+                //No devices were found
+                return;
+            }
+
+            //Get the device from its definition
+            var device = await hidFactory.GetDeviceAsync(deviceDefinitions.First()).ConfigureAwait(false);
+
+            //Initialize the device
+            await device.InitializeAsync().ConfigureAwait(false);
+
+            //Create the request buffer
+            var buffer = new byte[65];
+            buffer[0] = 0x00;
+            buffer[1] = 0x3f;
+            buffer[2] = 0x23;
+            buffer[3] = 0x23;
+
+            //Write and read the data to the device
+            var readBuffer = await device.WriteAndReadAsync(buffer).ConfigureAwait(false);
+            System.Diagnostics.Debug.WriteLine(readBuffer);
+        }
+    }
+}
