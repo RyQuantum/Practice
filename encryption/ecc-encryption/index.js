75b5ff79e8b16e489cfd047fd98904c9abbaba70
Ryan <ryan@rently.com>
2021-09-26 14:48:52 +0800

    update nodejs ecc script

--- encryption/ecc-encryption/index.js
+++ encryption/ecc-encryption/index.js
@@ -1,13 +1,29 @@
-const argon2 = require('argon2');
-const func = async () => {
-    try {
-        const hash = await argon2.hash("password");
-        console.log('hash:', hash);
-        const isCorrect = await argon2.verify(hash, "password");
-        console.log('isCorrect:', isCorrect);
-      } catch (err) {
-        console.log("error:", err);
-      }
-}
+const ECKey = require('ec-key');
+const crypto = require('crypto');
+const fs = require('fs');
+const net = require('net');
 
-func();
+const PIPE_NAME = "ecc_pipe";
+const PIPE_PATH = "\\\\.\\pipe\\" + PIPE_NAME;
+
+const serverPrivateKeyPem = fs.readFileSync('./openssl/server.key', { encoding:'utf8', flag:'r' });
+const clientPublicKeyPem = fs.readFileSync('./tmp/client_pub.key', { encoding:'utf8', flag:'r' });
+
+var serverKey = new ECKey(serverPrivateKeyPem, 'pem');
+var clientKey = new ECKey(clientPublicKeyPem, 'pem');
+
+let secret = serverKey.computeSecret(clientKey);
+
+secret = secret.slice(-8);
+console.log('secret:', secret);
+
+const md5 = crypto.createHash('md5');
+md5.update(secret);
+
+const client = net.createConnection(PIPE_PATH, () => {
+    //'connect' listener
+    console.log('connected to server!');
+    client.write('Secret: ' + secret.toString('hex').toUpperCase()); 
+    client.write(' AES: ' + md5.digest('hex').toUpperCase()); 
+    client.end();
+});
