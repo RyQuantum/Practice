985fda7370421e7936e4a11c83d95560f188f1b1
Ryan <ryan@rently.com>
2021-11-05 20:26:45 +0800

    add 88.Merge Sorted Array

---
+++ brushing/88.Merge Sorted Array.js
@@ -0,0 +1,23 @@
+var merge = function (nums1, m, nums2, n) {
+  while (m > 0 && n > 0) {
+    if (nums1[m - 1] > nums2[n - 1]) {
+      nums1[m + n - 1] = nums1[m - 1]
+      m--
+    } else {
+      nums1[m + n - 1] = nums2[n - 1]
+      n--
+    }
+  }
+  while (n > 0) {
+    nums1[n - 1] = nums2[n - 1]
+    n--
+  }
+};
+
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+
+merge(nums1, m, nums2, n)
+console.log(nums1)
